4df5850b03708f7d93bba5a57cf36a850d3e2b12
mansiemans <veratoe@gmail.com>
2016-05-26 22:28:38 +0200

    supertrage 1e versie

--- prime.c
+++ prime.c
@@ -1,22 +1,34 @@
-#include "stdio.h"
 #include <math.h>
+#include <time.h>
+#include <stdio.h>
 
-float is_prime(long long n) {
+float is_prime(long long n, int silent) {
+	int primality = 1;
 	long long int i;
-	for (i = 2; i < n; i++) {
-		//printf("%lli mod  %lli  %lli\n", n, i, n % i);
+	if (silent != 1) {
+		printf("We testen %lli\n", n);
+		printf("we gaan testen totaan: %f\n", sqrt(n));
+	}
+	for (i = 2; i < n / 2 + 1; i++) {
 		if (n % i == 0) {
-			return 0;
+			if (silent != 1) {
+				printf("factor: %lli", i);
+				if (is_prime(i, 1)) {
+					printf("!");
+				}
+				printf("\n");
+			}
+			primality = 0;
 		}
 	}
-	return 1;
+	return primality;
 }
 
 void print_factors(long long n) {
 	for (long long i = 2; i < n; i++) {
 		if (n % i == 0) {
 			printf("%lli", i);
-			if (is_prime(i)) printf("!");
+			if (is_prime(i, 0)) printf("!");
 			printf(" ");
 		}
 	}
@@ -33,7 +45,10 @@ void print_factors(long long n) {
 }*/
 
 int main(int argc, char *argv[]) {
+
 	long long int n = 1;
+	clock_t start = 0, stop = 0;
+
 	if (argc < 2) {
 		printf("Voer een getal in: ");
 		scanf("%lli", &n);	
@@ -41,10 +56,17 @@ int main(int argc, char *argv[]) {
 		n = *argv[1];
 		sscanf(argv[1], "%lli", &n);
 	}
-	if (is_prime(n) == 1) {
+
+	start = clock();
+
+	if (is_prime(n, 0) == 1) {
 		printf("%lli is een priem-getal\n", n);
 	} else {
-		printf("%lli is geen priem-getal en deelbaar door ", n);
-		print_factors(n);
+		printf("%lli is geen priem-getal", n);
+		//print_factors(n);
 	}
+
+	stop = clock();
+
+	printf("\nDit duurde %6.3f sec\n", (float) (stop - start) / CLOCKS_PER_SEC);
 }
